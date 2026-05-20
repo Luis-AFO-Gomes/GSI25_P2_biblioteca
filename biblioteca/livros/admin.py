63f73e2fe91f0ac76bf7d1cc3fdadcdb16cada50
Alexandre Gomes <alexandre_gomes@sapo.pt>
2026-05-20 18:11:45 +0100

    Branch correction

--- biblioteca/livros/admin.py
+++ biblioteca/livros/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Livro
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('isbn', 'titulo', 'idioma', 'tipo', 'tema', 'editora', 'data_pub', 'original', 'status', 'data_add')
+    search_fields = ('isbn', 'titulo')
+    list_filter = ('tipo', 'tema', 'status')
+    prepopulated_fields = {'tema': ('tipo',)}
--- biblioteca/livros/models.py
+++ biblioteca/livros/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Livro(models.Model):
+    class Status(models.TextChoices):
+        SUGERIDO = 'S', 'Sugerido'
+        DISPONIVEL = 'D', 'Disponível'
+        EMPRESTADO = 'E', 'Emprestado'
+        RESERVADO = 'R', 'Reservado'
+        RETIRADO = 'O', 'Retirado'
+    isbn = models.CharField(max_length=13, unique=True, primary_key=True)
+    titulo = models.CharField(max_length=200)
+    idioma = models.CharField(max_length=3)
+    tipo = models.CharField(max_length=50)
+    tema = models.CharField(max_length=100)
+    editora = models.CharField(max_length=100)
+    data_pub = models.DateField()
+    original = models.BooleanField(default=True)
+    status = models.CharField(
+        max_length=1,
+        choices=Status.choices,
+        default=Status.DISPONIVEL,
+    )
+    data_add = models.DateTimeField(default=timezone.now)
+    objects = models.Manager()          # gestor de modelos padrão
+    class Meta:
+        ordering = ['titulo']
+        indexes = [
+            models.Index(fields=['titulo']),
+            models.Index(fields=['tipo']),
+            models.Index(fields=['tema']),
+        ]
+
+    def __str__(self):
+        return self.titulo
