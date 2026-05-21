f5979a4f41d0cf8abe8d225a02642e8582d942ee
Alexandre Gomes <alexandre_gomes@sapo.pt>
2026-05-21 08:54:04 +0100

    correct .gitignore on main

--- biblioteca/livros/views.py
+++ biblioteca/livros/views.py
@@ -8,3 +8,7 @@ def home_livros(request):
 
 #def home_livros(request,nome='World'):
 #    return render(request, 'livros/home.html', {'title': 'Home', 'nome': nome, 'request': request})
+# uso: http://localhost:8000/livros/ ou http://localhost:8000/livros/«nome»
+# 1. reverte para default: nome = 'World'
+# 2. Apresenta o nome passado na URL: nome = '«nome»
+# - é necessário confirmar que existe um path correspondente (com parâmetro) em livros/urls.py
