84d551824061d594c5105274d5ddc83b4c34f9ad
Leonor Marques <a22508680@mso365.ipluso.pt>
2026-06-16 13:36:11 +0100

    Implementação das views CRUD para árvores

--- biblioteca/livros/views.py
+++ biblioteca/livros/views.py
@@ -3,131 +3,161 @@ from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView, UpdateView, View
 
-from .forms import LivroForm
-from .models import Livro
-from .services import LivroService
+from .forms import ArvoreForm
+from .models import Arvore
+from .services import ArvoreService
 
+class ArvoreListMixin:
 
-class LivroListMixin:
-    """Shared query behavior for pages that list Livro cards."""
-
-    model = Livro
-    context_object_name = 'livros'
+    model = Arvore
+    context_object_name = 'arvores'
 
     def get_queryset(self):
-        return LivroService.listar()
+        return ArvoreService.listar()
 
+class HomeArvoresView(ArvoreListMixin, ListView):
 
-class HomeLivrosView(LivroListMixin, ListView):
     template_name = 'livros/home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Home'
+        context['title'] = 'Venus07'
         return context
 
+class ListaArvoresView(ArvoreListMixin, ListView):
 
-class ListaLivrosView(LivroListMixin, ListView):
-    template_name = 'livros/livros.html'
+    template_name = 'livros/arvores.html'
 
+class ArvoreDetailView(DetailView):
 
-class LivroDetailView(DetailView):
-    model = Livro
-    template_name = 'livros/livro_form.html'
-    context_object_name = 'livro'
-    slug_field = 'isbn'
-    slug_url_kwarg = 'isbn'
+    model = Arvore
+    template_name = 'livros/arvore_form.html'
+    context_object_name = 'arvore'
 
     def get_object(self, queryset=None):
-        return LivroService.obter_por_isbn(self.kwargs['isbn'])
+        return ArvoreService.obter_por_id(
+            self.kwargs['pk']
+        )
 
     def get_context_data(self, **kwargs):
+
         context = super().get_context_data(**kwargs)
+
         context.update({
             'mode': 'detail',
-            'page_title': 'Detalhes do livro',
+            'page_title': 'Detalhes da árvore',
             'submit_label': None,
         })
+
         return context
 
 
-class LivroCreateView(CreateView):
-    model = Livro
-    form_class = LivroForm
-    template_name = 'livros/livro_form.html'
+class ArvoreCreateView(CreateView):
+
+    model = Arvore
+    form_class = ArvoreForm
+
+    template_name = 'livros/arvore_form.html'
 
     def get_context_data(self, **kwargs):
+
         context = super().get_context_data(**kwargs)
+
         context.update({
             'mode': 'create',
-            'page_title': 'Novo livro',
+            'page_title': 'Nova árvore',
             'submit_label': 'Inserir',
         })
+
         return context
 
     def form_valid(self, form):
+
         response = super().form_valid(form)
-        messages.success(self.request, 'Livro inserido com sucesso.')
+
+        messages.success(
+            self.request,
+            'Árvore inserida com sucesso.'
+        )
+
         return response
 
     def get_success_url(self):
-        return reverse_lazy('livros:detalhe_livro', kwargs={'isbn': self.object.isbn})
+
+        return reverse_lazy(
+            'livros:detalhe_arvore',
+            kwargs={'pk': self.object.id}
+        )
 
 
-class LivroUpdateView(UpdateView):
-    model = Livro
-    form_class = LivroForm
-    template_name = 'livros/livro_form.html'
-    context_object_name = 'livro'
-    slug_field = 'isbn'
-    slug_url_kwarg = 'isbn'
+class ArvoreUpdateView(UpdateView):
+
+    model = Arvore
+    form_class = ArvoreForm
+
+    template_name = 'livros/arvore_form.html'
+
+    context_object_name = 'arvore'
 
     def get_object(self, queryset=None):
-        return LivroService.obter_por_isbn(self.kwargs['isbn'])
 
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['readonly_isbn'] = True
-        return kwargs
+        return ArvoreService.obter_por_id(
+            self.kwargs['pk']
+        )
 
     def get_context_data(self, **kwargs):
+
         context = super().get_context_data(**kwargs)
+
         context.update({
             'mode': 'edit',
-            'page_title': 'Editar livro',
-            'submit_label': 'Guardar alteracoes',
+            'page_title': 'Editar árvore',
+            'submit_label': 'Guardar alterações',
         })
+
         return context
 
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        messages.success(self.request, 'Livro atualizado com sucesso.')
-        return response
-
     def get_success_url(self):
-        return reverse_lazy('livros:detalhe_livro', kwargs={'isbn': self.object.isbn})
+
+        return reverse_lazy(
+            'livros:detalhe_arvore',
+            kwargs={'pk': self.object.id}
+        )
 
 
-class LivroDeleteView(View):
-    template_name = 'livros/livro_confirm_delete.html'
+class ArvoreDeleteView(View):
 
-    def get_livro(self):
-        return LivroService.obter_por_isbn(self.kwargs['isbn'])
+    template_name = 'livros/arvore_confirm_delete.html'
+
+    def get_arvore(self):
+
+        return ArvoreService.obter_por_id(
+            self.kwargs['pk']
+        )
 
     def get(self, request, *args, **kwargs):
-        livro = self.get_livro()
-        return render(request, self.template_name, {'livro': livro})
+
+        arvore = self.get_arvore()
+
+        return render(
+            request,
+            self.template_name,
+            {'arvore': arvore}
+        )
 
     def post(self, request, *args, **kwargs):
-        livro = self.get_livro()
-        titulo = livro.titulo
-        LivroService.apagar(livro)
-        messages.success(request, f'Livro "{titulo}" apagado com sucesso.')
-        return redirect('livros:lista_livros')
 
+        arvore = self.get_arvore()
 
-def page_not_found(request, exception):
-    """Render project 404 errors inside the normal site layout."""
+        nome = arvore.nome
 
+        ArvoreService.apagar(arvore)
+
+        messages.success(
+            request,
+            f'Árvore "{nome}" apagada com sucesso.'
+        )
+
+        return redirect('livros:lista_arvores')
     message = str(exception) if exception else 'Pagina nao encontrada.'
     return render(request, 'livros/404.html', {'message': message}, status=404)
