e16eb8d35bc049c4f0275b707fe82ddafecb2cd1
Leonor Marques <a22508680@mso365.ipluso.pt>
2026-06-16 13:38:09 +0100

    Adaptação das URLs para o sistema Venus07

--- biblioteca/livros/urls.py
+++ biblioteca/livros/urls.py
@@ -2,11 +2,43 @@ from django.urls import path
 from . import views
 
 app_name = 'livros'
+
 urlpatterns = [
-    path('', views.HomeLivrosView.as_view(), name='home_livros'),
-    path('catalogo/', views.ListaLivrosView.as_view(), name='lista_livros'),
-    path('livro/novo/', views.LivroCreateView.as_view(), name='criar_livro'),
-    path('livro/<str:isbn>/', views.LivroDetailView.as_view(), name='detalhe_livro'),
-    path('livro/<str:isbn>/editar/', views.LivroUpdateView.as_view(), name='editar_livro'),
-    path('livro/<str:isbn>/apagar/', views.LivroDeleteView.as_view(), name='apagar_livro'),
+
+    path(
+        '',
+        views.HomeArvoresView.as_view(),
+        name='home_arvores'
+    ),
+
+    path(
+        'catalogo/',
+        views.ListaArvoresView.as_view(),
+        name='lista_arvores'
+    ),
+
+    path(
+        'arvore/nova/',
+        views.ArvoreCreateView.as_view(),
+        name='criar_arvore'
+    ),
+
+    path(
+        'arvore/<int:pk>/',
+        views.ArvoreDetailView.as_view(),
+        name='detalhe_arvore'
+    ),
+
+    path(
+        'arvore/<int:pk>/editar/',
+        views.ArvoreUpdateView.as_view(),
+        name='editar_arvore'
+    ),
+
+    path(
+        'arvore/<int:pk>/apagar/',
+        views.ArvoreDeleteView.as_view(),
+        name='apagar_arvore'
+    ),
+
 ]
