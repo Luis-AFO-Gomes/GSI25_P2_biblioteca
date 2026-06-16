bc65459408e853ff95600a5c90b64c93e62f46fe
Leonor Marques <a22508680@mso365.ipluso.pt>
2026-06-16 13:41:29 +0100

    Configuração do painel de administração para árvores

--- biblioteca/livros/admin.py
+++ biblioteca/livros/admin.py
@@ -1,38 +1,27 @@
 from django.contrib import admin
 
-from .models import Autor, Editora, Livro, Tema, ZonasGeograficas
-@admin.register(Editora)
-class EditoraAdmin(admin.ModelAdmin):
-    list_display = ('nipc', 'nome', 'contacto', 'morada')
-    search_fields = ('nipc', 'nome')
-    list_filter = ('nome',)
-
-@admin.register(Livro)
-class LivroAdmin(admin.ModelAdmin):
-    list_display = ('isbn', 'titulo', 'get_autores', 'idioma', 'tipo', 'tema', 'editora', 'data_pub', 'original', 'status', 'data_add')
-    search_fields = ('isbn', 'titulo')
-    list_filter = ('tipo', 'tema', 'status')
-
-    @admin.display(description='Autores')
-    def get_autores(self, obj):
-        return obj.get_autores()
-
-@admin.register(Tema)
-class TemaAdmin(admin.ModelAdmin):  
-    list_display = ('sigla', 'nome')
-    search_fields = ('nome',)
-    list_filter = ('nome',)
-
-@admin.register(Autor)
-class AutorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'data_nasc', 'nacionalidade')
-    search_fields = ('id', 'nome')
-    list_filter = ('nacionalidade',)   
-
-@admin.register(ZonasGeograficas)
-class ZonasGeograficasAdmin(admin.ModelAdmin):
-    list_display = ('sigla', 'nome')
-    search_fields = ('sigla', 'nome')
-    list_filter = ('nome',)     
+from .models import Arvore
 
 
+@admin.register(Arvore)
+class ArvoreAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'nome',
+        'tipo',
+        'altura',
+        'preco',
+        'stock',
+        'pais_origem'
+    )
+
+    search_fields = (
+        'nome',
+        'tipo',
+        'pais_origem'
+    )
+
+    list_filter = (
+        'tipo',
+        'pais_origem'
+    )
